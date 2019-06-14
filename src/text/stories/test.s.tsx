f7682d8f9e9e327a8353b14528c0e3583de33549
le <445220716@qq.com>
2019-06-14 23:09:17 +0800

    remove test

--- src/text/stories/test.s.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-import { test } from '../';
-
-storiesOf('Components/Button', module).add('basic PrimaryButton', () => (
-  <button>{test}</button>
-));

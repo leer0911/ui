b2539922ebe9704589a8a36db30aa23f03a53ee3
le <445220716@qq.com>
2019-06-16 22:09:09 +0800

    add ts config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,6 +2,15 @@ const path = require('path');
 
 module.exports = ({ config }) => {
   config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve('awesome-typescript-loader'),
+      },
+    ],
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+  config.module.rules.push({
     test: /\.less$/,
     use: [
       'style-loader',
